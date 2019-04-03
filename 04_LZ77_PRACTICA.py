38aed4b1370c37029b3ed57395ee6f31711f772d
Marco <marco.terral@hotmail.com>
2019-04-03 18:24:40 +0200

    actualizado

--- 04_LZ77_PRACTICA.py
+++ 04_LZ77_PRACTICA.py
@@ -6,14 +6,53 @@ Dado un mensaje, el tamaño de la ventana de trabajo L, y el tamaño
 del buffer de búsqueda S dar la codificación del mensaje usando el
 algoritmo LZ77
 
-
-mensaje='cabracadabrarrarr'
-LZ77Code(mensaje,12,6)=['c', 0, 0], ['a', 0, 0],  ['b', 0, 0],
- ['r', 0, 0],  ['c', 1, 3],  ['d', 1, 2], ['r', 4, 7],  ['EOF', 4, 3]]
-  
-  
-[símbolo, longitud_máxima_cadena, posición]    
 """
+  
+  
+#[símbolo, longitud_máxima_cadena, posición]    
+
+
+def find(bfT,bfB,caracter):
+    
+    tripleta=[bfT[0],0,0]
+    l=0
+    usoCaracter=False
+    resbfT=bfT[1:]
+    resbfB=bfB+bfT[0]
+    
+    for i in range(0,len(bfB)) :
+    
+        ltmp=0
+        tmpoffset=len(bfB)-i
+        j=i
+        tmpbfT=bfT
+        tmpbfB=bfB
+        
+        while j < len(tmpbfB) and len(tmpbfT)>0 and tmpbfB[j]==tmpbfT[0] :
+            tmpbfB=tmpbfB+tmpbfT[0]
+            tmpbfT=tmpbfT[1:]
+            j+=1
+            ltmp+=1
+
+        if ltmp>l or (ltmp==l and tripleta[2]>tmpoffset):
+            if len(tmpbfT) == 0:
+                tripleta=[caracter,ltmp,tmpoffset]
+                usoCaracter=True
+                l=ltmp
+                resbfT=tmpbfT
+                resbfB=tmpbfB
+            else:
+                tripleta=[tmpbfT[0],ltmp,tmpoffset]
+                usoCaracter=False
+                l=ltmp
+                resbfB=tmpbfB+tmpbfT[0]
+                resbfT=tmpbfT[1:]
+            print
+    
+    
+    return (tripleta,resbfT,resbfB,usoCaracter)
+               
+#print(find("rarrad","adabrar","EOF"))
 
 def LZ77Code(mensaje,S=12,L=6):
     bufferbusq=""
@@ -21,10 +60,14 @@ def LZ77Code(mensaje,S=12,L=6):
     tmpmsn=mensaje[L:]
     result=[]
     while len(buffertrab) > 0:
-
-        tripleta,buffertrab,bufferbusq=find(buffertrab,bufferbusq)
-        result=result+tripleta
-
+        if len(tmpmsn)==0:
+            char="EOF"
+        else:
+            char=tmpmsn[0]
+        tripleta,buffertrab,bufferbusq,usChar=find(buffertrab,bufferbusq,char)
+        result=result+[tripleta]
+        if usChar and len(tmpmsn)>0:
+            bufferbusq+=tmpmsn[0]
         if len(bufferbusq) > S :
             x = len(bufferbusq) - S
             bufferbusq=bufferbusq[x:]
@@ -34,8 +77,16 @@ def LZ77Code(mensaje,S=12,L=6):
             buffertrab=buffertrab+tmpmsn[:x]
             tmpmsn=tmpmsn[x:]
 
-    if result[len(result)-1][2] != "EOF" :
-        result=result + ["EOF",0,0]
+    if result[len(result)-1][0] != "EOF" :
+        result=result + [["EOF",0,0]]
+
+    return result
+
+mensaje='patadecabra'
+print(LZ77Code(mensaje,12,6))
+
+#['c', 0, 0], ['a', 0, 0],  ['b', 0, 0],
+#['r', 0, 0],  ['c', 1, 3],  ['d', 1, 2], ['r', 4, 7],  ['EOF', 4, 3]]
 """
 Dado un mensaje codificado con el algoritmo LZ77 hallar el mensaje 
 correspondiente 
@@ -47,16 +98,27 @@ LZ77Decode(code)='patadecabra'
 
 """   
 def LZ77Decode(codigo):
-
     
+    result=""
+    for t in codigo:
+        pos=t[2]
+        length=t[1]
+        pos=len(result)-pos
+        while length > 0 :
+            result=result+result[pos]
+            pos=pos+1
+            length=length-1
+        if t[0]!="EOF" :
+            result=result+t[0]
     
-    
+    return result
 
-
-"""
-Jugar con los valores de S y L (bits_o y bits_l)
-para ver sus efectos (tiempo, tamaño...)
-"""
+#code=[['c', 0, 0], ['a', 0, 0],  ['b', 0, 0],['r', 0, 0],  ['c', 1, 3],  ['d', 1, 2], ['r', 4, 7],  ['EOF', 4, 3]]
+#print(LZ77Decode(code))
+#"""
+#Jugar con los valores de S y L (bits_o y bits_l)
+#para ver sus efectos (tiempo, tamaño...)
+#"""
 
 
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos. La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos. La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos. La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos.'
